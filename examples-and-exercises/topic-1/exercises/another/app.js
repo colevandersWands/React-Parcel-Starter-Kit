464091646099425e7cafaa8269943b1b69eb164e
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-11-26 08:28:57 +0100

    module practice poc

---
+++ examples-and-exercises/topic-1/exercises/another/app.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const name = 'Ben Chacko';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>asdf {name}!</h1>
+    </Fragment>
+  );
+};
+
+export default App;
